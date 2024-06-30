e13783774f73c8cfef250135224e8b180e963ec5
AungKoLin828 <aungko.linn404@gmail.com>
2024-06-30 16:04:07 +0630

    Initial commit

---
+++ src/main/java/com/mm/websocket/WebSocketOpenAiAppApplication.java
@@ -0,0 +1,13 @@
+package com.mm.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketOpenAiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketOpenAiAppApplication.class, args);
+	}
+
+}
